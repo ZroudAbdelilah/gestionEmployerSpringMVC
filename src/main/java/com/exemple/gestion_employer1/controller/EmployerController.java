504ec77af1fa9c90470b7c03d4bdf61522e22444
ZroudAbdelilah <76938527+ZroudAbdelilah@users.noreply.github.com>
2022-02-23 12:35:33 +0100

    corriger la configuration add admin repo

--- src/main/java/com/exemple/gestion_employer1/controller/EmployerController.java
+++ src/main/java/com/exemple/gestion_employer1/controller/EmployerController.java
@@ -1,16 +1,74 @@
 package com.exemple.gestion_employer1.controller;
 
+
+import com.exemple.gestion_employer1.dao.AdminRepoImp;
+import com.exemple.gestion_employer1.dao.AdminRepoInt;
+import com.exemple.gestion_employer1.dao.DAOInt;
+import com.exemple.gestion_employer1.dao.EmployerDaoImp;
+import com.exemple.gestion_employer1.entity.EmployerEntity;
+import com.exemple.gestion_employer1.services.ServiceInt;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 @Controller
+@RequestMapping("/")
 public class EmployerController {
 
-    @RequestMapping("/add")
-    @ResponseBody
-    public String addEmp(){
-        return "hello";
+    private ServiceInt<EmployerEntity> serviceEmployer;
+
+    @Autowired
+    public EmployerController(ServiceInt<EmployerEntity> serviceEmployer) {
+        this.serviceEmployer = serviceEmployer;
     }
+
+    @GetMapping("list")
+    public String listUser (Model theModel){
+        List <EmployerEntity> theEmployers = serviceEmployer.getAll();
+        return "listeEmployer";
+    }
+
+    @GetMapping("new")
+    public String showNewForm(Model theModel,HttpServletRequest request){
+        EmployerEntity theEmployer = new EmployerEntity();
+        theModel.addAttribute("employer",theEmployer);
+        return "addEmployer";
+    }
+    @PostMapping("insert")
+    public String insertUser(@ModelAttribute("employer") EmployerEntity theEmployer){
+        serviceEmployer.add(theEmployer);
+        return "redirect:list";
+    }
+    @GetMapping("edit")
+    public String showEditForm(@RequestParam("id_user") Long theId,Model theModel){
+        EmployerEntity theEmployer = serviceEmployer.find(theId);
+        theModel.addAttribute("employer",theEmployer);
+        return "editeUser";
+    }
+    @GetMapping("delete")
+    public String deleteUser(@RequestParam("user_id") Long theId){
+        serviceEmployer.delete(theId);
+        return "redirect:list";
+    }
+
+
+
+//    @RequestMapping("/add")
+//    @ResponseBody
+//    public String addEmp(){
+//        return "hello";
+//    }
+
 
 }
